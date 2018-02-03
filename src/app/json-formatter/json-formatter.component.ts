9499c747d0649045b3f51a18f8f0f4d6cdff6737
Sreenivasa Reddy <sreenivasajava26@gmail.com>
2018-02-03 01:05:59 -0800

    Json formatter and validation support added. Also addedd code to handle errors while formatting json

--- src/app/json-formatter/json-formatter.component.ts
+++ src/app/json-formatter/json-formatter.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JsonFormatterComponent implements OnInit {
 
+  jsonString;
+  invalid = false;
+  success = false;
+  errorMessage;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  formatJson(jsonRaw) {
+    try {
+        this.jsonString = JSON.parse(jsonRaw);
+        this.invalid = false;
+        this.success = true;
+    } catch(e) {
+      this.invalid = true;
+      this.success = false;
+      this.errorMessage = e;
+    }
+
   }
 
 }
